22a6546f16f50aa639fd21bef751cfcffacffa05
Fikri Haykal <fikrihaykal@its.ac.id>
2023-05-30 09:56:13 +0700

    feat(pages): creating few pages for transition

---
+++ src/pages/komponen/index.tsx
@@ -0,0 +1,21 @@
+import BaseLayout from "@/components/BaseLayout"
+import { Box, Heading, Image, Stack, Text } from "@chakra-ui/react"
+
+const Komponen = () => {
+	return (
+		<>
+			<BaseLayout>
+				<Box as="section" id="dashboard-hero-section" pb={{ base: '4', md: '8' }}>
+					<Stack direction={{ base: 'column', sm: 'row' }} justifyContent="space-between" alignItems="center">
+						<Stack justifyContent="center" h="250px">
+							<Heading>Komponen</Heading>
+						</Stack>
+						<Image display={{ base: 'none', lg: 'block' }} src="images/app/myITS-DesignSystem.svg" h="200px" />
+					</Stack>
+				</Box>
+			</BaseLayout>
+		</>
+	)
+}
+
+export default Komponen
