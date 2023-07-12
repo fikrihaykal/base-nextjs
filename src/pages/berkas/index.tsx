cb8dd37ee1ad973be61273753706f44012965764
nashirat <nashir1187@gmail.com>
2023-07-12 11:23:16 +0700

    berkas: new table ui

--- src/pages/berkas/index.tsx
+++ src/pages/berkas/index.tsx
@@ -1,5 +1,9 @@
 import PageTransition from "@/components/PageLayout";
-import { BoxIconMade, EditIconMade, SearchIconMade } from "@/components/atoms/IconsMade";
+import {
+  BoxIconMade,
+  EditIconMade,
+  SearchIconMade,
+} from "@/components/atoms/IconsMade";
 import MenuWrapper from "@/components/atoms/MenuWrapper";
 import CardImage from "@/components/molecules/CardImage";
 import CardIconShadow from "@/components/organisms/CardIconShadow";
@@ -9,14 +13,17 @@ import RightMenu from "@/components/organisms/RightMenu";
 import {
   Box,
   Button,
+  Checkbox,
   Flex,
   Heading,
   Image,
   Input,
   Stack,
+  Table,
   Text,
   useColorMode,
 } from "@chakra-ui/react";
+import { useState } from "react";
 
 const Berkas = () => {
   const { colorMode } = useColorMode();
@@ -24,6 +31,16 @@ const Berkas = () => {
   const bgDark = "/images/app/card/backgrounddark.png";
   const cover = "/images/app/card/cover.png";
   const coverdark = "/images/app/card/coverdark.png";
+
+  const [checked, setChecked] = useState(true);
+
+  const setCheckedVal = () => {
+    if (checked) {
+      setChecked(false);
+    } else if (!checked) {
+      setChecked(true);
+    }
+  };
 
   return (
     <>
@@ -135,7 +152,7 @@ const Berkas = () => {
                               "transform .25s, -webkit-transform .25s, -moz-transform .25s",
                           }}
                         >
-                          Image
+                          Sertifikat
                         </Flex>
                       </Box>
                     </Flex>
@@ -188,12 +205,12 @@ const Berkas = () => {
                           color="#1b1d21"
                           placeholder="Search"
                           _focusVisible={{
-                            border: "none"
+                            border: "none",
                           }}
                         ></Input>
                       </Box>
                       <Flex className="sorting__search" align-items="center">
-                      <Button
+                        <Button
                           className="sorting__action"
                           flexShrink="0"
                           w="48px"
@@ -206,7 +223,7 @@ const Berkas = () => {
                             boxShadow: "0 5px 20px rgba(227, 230, 236, 0.85)",
                           }}
                           _notLast={{
-                            marginRight: "24px"
+                            marginRight: "24px",
                           }}
                         >
                           <EditIconMade
@@ -229,7 +246,7 @@ const Berkas = () => {
                             boxShadow: "0 5px 20px rgba(227, 230, 236, 0.85)",
                           }}
                           _notLast={{
-                            marginRight: "24px"
+                            marginRight: "24px",
                           }}
                         >
                           <BoxIconMade
@@ -239,11 +256,153 @@ const Berkas = () => {
                             h="1em"
                           ></BoxIconMade>
                         </Button>
-                        
                       </Flex>
                     </Flex>
                   </Box>
                 </Flex>
+              </Box>
+              <Box className="table__container">
+                <Box className="table__main" display="table" w="100%">
+                  <Box className="table__row head" display="table-row">
+                    <Box
+                      className="table__cell head"
+                      display="table-cell"
+                      verticalAlign="middle"
+                      borderBottom="1px solid #e4e4e4"
+                      paddingTop="24px"
+                      paddingBottom="24px"
+                      fontSize="13px"
+                      lineHeight="1.38462"
+                      fontWeight="500"
+                      color="#b2b3BD"
+                      _first={{
+                        width: "20px",
+                        padding: "0",
+                        fontSize: "0",
+                      }}
+                    >
+                      <Box
+                        className="checkbox__custom"
+                        display="inline-block"
+                        pos="relative"
+                        userSelect="none"
+                        cursor="pointer"
+                      >
+                        <Checkbox
+                          className="checkbox__input"
+                          pos="absolute"
+                          top="0"
+                          left="0"
+                          opacity="0"
+                        ></Checkbox>
+                        <Flex className="checkbox__in">
+                          <Flex
+                            className="checkbox__tick"
+                            pos="relative"
+                            flex="0 0 20px"
+                            w="20px"
+                            h="20px"
+                            borderRadius="4px"
+                            border="2px solid #e4e4e4"
+                            transition="all .25s"
+                            onClick={setCheckedVal}
+                            bg={checked ? "#008fff" : "transparent"}
+                            borderColor={checked ? "#008fff" : "#e4e4e4"}
+                            _before={{
+                              content: '""',
+                              pos: "absolute",
+                              top: "50%",
+                              left: "50%",
+                              transform: "translate(-50%, -50%)",
+                              width: "10px",
+                              height: "9px",
+                              backgroundImage: `url(/check.svg)`,
+                            }}
+                            _hover={{
+                              borderColor: "#008Fff",
+                            }}
+                          ></Flex>
+                        </Flex>
+                      </Box>
+                    </Box>
+                    <Box
+                      className="table__cell head"
+                      display="table-cell"
+                      verticalAlign="middle"
+                      borderBottom="1px solid #e4e4e4"
+                      paddingTop="24px"
+                      paddingBottom="24px"
+                      fontSize="13px"
+                      lineHeight="1.38462"
+                      fontWeight="500"
+                      color="#b2b3BD"
+                      _first={{
+                        width: "20px",
+                        padding: "0",
+                        fontSize: "0",
+                      }}
+                    >
+                      File
+                    </Box>
+                    <Box
+                      className="table__cell head"
+                      display="table-cell"
+                      verticalAlign="middle"
+                      borderBottom="1px solid #e4e4e4"
+                      paddingTop="24px"
+                      paddingBottom="24px"
+                      fontSize="13px"
+                      lineHeight="1.38462"
+                      fontWeight="500"
+                      color="#b2b3BD"
+                      _first={{
+                        width: "20px",
+                        padding: "0",
+                        fontSize: "0",
+                      }}
+                    >
+                      Jenis
+                    </Box>
+                    <Box
+                      className="table__cell head"
+                      display="table-cell"
+                      verticalAlign="middle"
+                      borderBottom="1px solid #e4e4e4"
+                      paddingTop="24px"
+                      paddingBottom="24px"
+                      fontSize="13px"
+                      lineHeight="1.38462"
+                      fontWeight="500"
+                      color="#b2b3BD"
+                      _first={{
+                        width: "20px",
+                        padding: "0",
+                        fontSize: "0",
+                      }}
+                    >
+                      Tanggal
+                    </Box>
+                    <Box
+                      className="table__cell head"
+                      display="table-cell"
+                      verticalAlign="middle"
+                      borderBottom="1px solid #e4e4e4"
+                      paddingTop="24px"
+                      paddingBottom="24px"
+                      fontSize="13px"
+                      lineHeight="1.38462"
+                      fontWeight="500"
+                      color="#b2b3BD"
+                      _first={{
+                        width: "20px",
+                        padding: "0",
+                        fontSize: "0",
+                      }}
+                    >
+                      Keterangan
+                    </Box>
+                  </Box>
+                </Box>
               </Box>
             </Box>
           </MenuWrapper>
