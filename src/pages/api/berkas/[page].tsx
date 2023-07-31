7c00207fa1f9220c28756c3a78b499dec5da4fc8
nashirat <nashir1187@gmail.com>
2023-07-31 08:15:09 +0700

    berkas: theme

--- src/pages/api/berkas/[page].tsx
+++ src/pages/api/berkas/[page].tsx
@@ -17,7 +17,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
                     total: 12,
                 },
                 data: [
-                    dataTabelBerkas[page % 2]
+                    dataTabelBerkas[page%2]
                 ]
             }
             return res.status(200).json(response)
