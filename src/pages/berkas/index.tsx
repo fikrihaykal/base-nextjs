0a05e3187a89cc0f94e7e1c5ddb55d2e1f83bb70
nashirat <nashir1187@gmail.com>
2023-08-18 10:35:25 +0700

    modal: styling and animation done

--- src/pages/berkas/index.tsx
+++ src/pages/berkas/index.tsx
@@ -1,8 +1,8 @@
 import PageTransition from "@/components/PageLayout";
 import { BoxIconMade, EditIconMade } from "@/components/atoms/IconsMade";
 import MenuWrapper from "@/components/atoms/MenuWrapper";
-import { Flex, Button } from "@chakra-ui/react";
-import { useState } from "react";
+import { Flex, Button, Box } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 import { DropdownItem, DropdownItemDate } from "@/data/dummy";
 import {
   TableSearch,
@@ -18,9 +18,57 @@ import { kolomTabelBerkas } from "@/data/table";
 import { ButtonIcon } from "@/components/molecules/Button";
 import { TableInfinite } from "@/components/organisms/TableInfinite";
 import { infiniteQuery, tableLoadMoreConf } from "@/utils/table";
+import { MotionBox } from "@/components/motion/Motion";
+import { useCycle } from "framer-motion";
+
+const modalVariants = {
+  open: {
+    y: 0,
+    transition: {
+      delay: 0.15,
+      type: "spring",
+      duration: 0.4,
+      bounce: 0.28,
+    },
+  },
+  closed: {
+    y: "-130%",
+    transition: {
+      type: "spring",
+      duration: 0.4,
+      bounce: 0.28,
+    },
+  },
+};
+
+const modalBg = {
+  open: {
+    display: "flex",
+    opacity: 1,
+    transition: {
+      duration: 0.2,
+    },
+  },
+
+  closed: {
+    opacity: 0,
+    transition: {
+      duration: 0.2,
+    },
+    transitionEnd: { display: "none" },
+  },
+};
 
 const Berkas = () => {
   const [globalFilter, setGlobalFilter] = useState("");
+  const [modalActive, setModalActive] = useCycle(false, true);
+
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const handleScroll = () => {
+    const position = window.scrollY;
+    console.log(position);
+    setScrollPosition(position);
+  };
 
   const URL = "/api/berkas";
   const infiniteData = infiniteQuery(URL, "berkas");
@@ -30,6 +78,20 @@ const Berkas = () => {
     globalFilter,
     setGlobalFilter
   );
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll, { passive: true });
+
+    return () => {
+        window.removeEventListener('scroll', handleScroll);
+    };
+});
+
+  useEffect(() => {
+    modalActive
+      ? (document.body.style.overflow = "hidden")
+      : (document.body.style.overflow = "auto");
+  }, [modalActive]);
 
   return (
     <>
@@ -67,7 +129,7 @@ const Berkas = () => {
                         align-items="center"
                         w={{ base: "100%", s: "unset" }}
                       >
-                        {/* -----PRIMARY BUTTON----- */}
+                        {/* MODAL BUTTON */}
                         <Button
                           className="button__more"
                           // bg="#1b1b1b"
@@ -85,6 +147,7 @@ const Berkas = () => {
                           fontWeight="700"
                           transition="all .25s"
                           bg="#008fff"
+                          onClick={() => setModalActive(1)}
                           // _hover={{
                           //   background:
                           //     colorMode == "light" ? "#008fff" : "#0071ca",
@@ -109,6 +172,42 @@ const Berkas = () => {
           </MenuWrapper>
         </Flex>
       </PageTransition>
+
+      {/* MODAL */}
+      <MotionBox
+        display="none"
+        pos="absolute"
+        w="100vw"
+        h="100%"
+        bg="rgba(0,0,0,0.6)"
+        top="0"
+        left="0"
+        // top= "50%"
+        // left= "50%"
+        // transform= "translate(-50%, -50%)"
+        zIndex="99"
+        overflow="none"
+        justifyContent="center"
+        pt="120px"
+        onClick={() => setModalActive(0)}
+        variants={modalBg}
+        animate={modalActive ? "open" : "closed"}
+      >
+        <MotionBox
+          pos="relative"
+          w="600px"
+          h="400"
+          p="20px"
+          borderRadius="24px"
+          bg="white"
+          zIndex="99"
+          variants={modalVariants}
+          animate={modalActive ? "open" : "closed"}
+        >
+          {/* child over here */}
+        </MotionBox>
+      </MotionBox>
+      {/* MODAL END */}
     </>
   );
 };
